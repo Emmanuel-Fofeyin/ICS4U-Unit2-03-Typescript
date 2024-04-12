f81fd787f06c9c9a2480694cf1eb133f65adfbcb
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-04-12 09:19:26 -0400

    done

---
+++ EmmanuelStack.ts
@@ -0,0 +1,65 @@
+/**
+ * The EmmanuelStack Class.
+ *
+ * author  Emmanuel.FN
+ * version 1.0
+ * since   2024-04-12
+ */
+
+export class EmmanuelStack {
+
+    /*
+    * The constructor for the EmmanuelStack string stack class.
+    */
+    constructor() {
+      this.stackAsList = []
+    }
+  
+    /*
+    * Checks if stack is empty.
+    */
+    public get isEmpty() {
+      return this.stackAsList.length == 0
+    }
+  
+    /*
+    * returns stack size
+    */
+    public get size() {
+      return this.stackAsList.length
+    }
+    
+    /*
+    * returns the stack as a string
+    */
+    public get showStack() {
+      let values = ""
+      for (let counter = 0; counter < this.stackAsList.length; counter++) {
+        values = values + this.stackAsList[counter] + ", "
+      }
+      values = values.substring(0, values.length-2)
+      return values
+    }
+  
+    /*
+    * pushes a string onto the stack
+    */
+    public push(input) {
+      this.stackAsList.push(input)
+    }
+  
+    /*
+    * pops a string from the stack
+    */
+    public popItem() {
+      let poppedItem = ""
+      if (this.stackAsList[0]) {
+        poppedItem = this.stackAsList[this.stackAsList.length - 1]
+      } else {
+        poppedItem = "nothing to remove"
+      }
+      this.stackAsList.splice(-1)
+      return poppedItem
+    }
+  }
+  
--- index.ts
+++ index.ts
@@ -1,1 +1,47 @@
-console.log("Hello via Bun!");
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author  Emmanuel.FN
+ * version 1.0
+ * since   2024-04-12
+ */
+import { EmmanuelStack } from './EmmanuelStack'
+
+let fruits = new EmmanuelStack()
+fruits.push("apple")
+fruits.push("banana")
+fruits.push("cherry")
+console.log("fruits: " + fruits.showStack)
+console.log("popped: " + fruits.popItem())
+console.log("fruits: " + fruits.showStack)
+console.log("length: " + fruits.size)
+console.log("is empty: " + fruits.isEmpty)
+console.log("")
+
+let colours = new EmmanuelStack()
+colours.push("red")
+colours.push("green")
+colours.push("blue")
+console.log("colours: " + colours.showStack)
+console.log("popped: " + colours.popItem())
+console.log("popped: " + colours.popItem())
+console.log("colours: " + colours.showStack)
+console.log("length: " + colours.size)
+console.log("is empty: " + colours.isEmpty)
+console.log("")
+
+let shapes = new EmmanuelStack()
+shapes.push("square")
+shapes.push("triangle")
+shapes.push("circle")
+console.log("shapes: " + shapes.showStack)
+console.log("popped: " + shapes.popItem())
+console.log("popped: " + shapes.popItem())
+console.log("popped: " + shapes.popItem())
+console.log("popped: " + shapes.popItem())
+console.log("shapes: " + shapes.showStack)
+console.log("length: " + shapes.size)
+console.log("is empty: " + shapes.isEmpty)
+console.log("")
+
+console.log("\nDone.")

8714cfd484ba667a327b2708a8ea3cd36aa5391c
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-05-25 14:44:33 -0400

    done

--- EmmanuelStack.ts
+++ EmmanuelStack.ts
@@ -18,7 +18,7 @@ export class EmmanuelStack {
     /*
     * Checks if stack is empty.
     */
-    public get isEmpty(): boolean {     
+    public get isEmpty() {     
       return this.stackAsList.length == 0
     }
   
